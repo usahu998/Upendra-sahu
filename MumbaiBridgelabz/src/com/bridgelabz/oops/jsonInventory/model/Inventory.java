d2dfe4fbf7744c1eb42bae57e9285b8302630d8f
usahu998 <48016629+usahu998@users.noreply.github.com>
2019-09-03 18:00:42 +0530

    Add files via upload

---
+++ MumbaiBridgelabz/src/com/bridgelabz/oops/jsonInventory/model/Inventory.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.oops.jsonInventory.model;
+public class Inventory
+{
+	private String inventory;
+	private String name;
+	private long weight;
+	private long price;
+	
+	public String getInventory() {
+		return inventory;
+	}
+	public void setInventory(String inventory) {
+		this.inventory = inventory;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getWeight() {
+		return weight;
+	}
+	public void setWeight(long weight) {
+		this.weight = weight;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	
+	
+	
+
+}
